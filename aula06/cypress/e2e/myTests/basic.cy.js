d10b1195fb172b607e5bf9a5e6ba08328928478f
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-06-22 17:32:45 -0300

    .pause() and .debug()

---
+++ aula06/cypress/e2e/myTests/basic.cy.js
@@ -0,0 +1,32 @@
+/// <reference types="Cypress" />
+
+describe('Cypress basics', () => {
+    it('Should visit a page and assert title', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html')
+
+        cy.title().should('be.equal', 'Campo de Treinamento');//assertion
+
+        cy.title().should('contain', 'Campo');//assertion
+
+        cy.title().should('include', 'Campo');//assertion
+
+        cy.title()
+            .should('be.equal', 'Campo de Treinamento').debug()//assertion
+            .and('contain', 'Campo');//assertion
+
+        // cy.title().should('be.equal','Campo de ');//assertion
+
+        
+    });
+
+    it('Should find and interact with an element', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+        cy.pause();
+        cy.get('#buttonSimple')
+        .click()
+        .should('have.value', 'Obrigado!'); 
+
+
+    });
+
+}); 
