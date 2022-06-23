21dd30f7e7106fc60344d4bb194b390948739c36
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-06-22 21:24:18 -0300

    class finish. description: check text in website

---
+++ aula07/cypress/e2e/myTests/elements.cy.js
@@ -0,0 +1,12 @@
+/// <reference types="Cypress" />
+
+describe('Work with basic elements', () => {
+    it('Text', () => {
+        cy.visit('https://www.wcaquino.me/cypress/componentes.html');
+        cy.get('body').should('contain', 'Cuidado');
+        cy.get('span').should('contain', 'Cuidado');
+        cy.get('.facilAchar').should('contain','Cuidado')
+        cy.get('.facilAchar').should('have.text','Cuidado onde clica, muitas armadilhas...')
+    });
+
+});
