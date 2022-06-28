cd226ef974bb868566f1ce981467dae3ce44f7d2
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-06-28 17:59:19 -0300

    ....

---
+++ aula10/cypress/e2e/myTests/barriga/functional.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="Cypress" />
+
+describe('Work with alerts', () => {
+    before(() => { //before all tests
+         // hook
+
+    });
+
+    it('cadastro', () => {
+        cy.visit('https://barrigareact.wcaquino.me');
+
+        cy.get(':nth-child(2) > .nav-link').click();
+
+        cy.get('.jumbotron > :nth-child(1) > .form-control').type('Rafael Araújo da Silva');
+
+        cy.get('.input-group > .form-control').type('ralfsniper0102@gmail.com');
+
+        cy.get(':nth-child(3) > .form-control').type('123456');
+
+        cy.get('.btn').click();
+    });
+
+    it.only('login', () => {
+        cy.visit('https://barrigareact.wcaquino.me/');
+
+        cy.get('[data-test="email"]').type('ralfsniper0102@gmail.com');
+        cy.get('[data-test="passwd"]').type('123456');
+
+        cy.get('.btn').click();
+
+        cy.get('.toast-message').should('exist');
+        cy.get('.toast-message').should('contain', 'Bem vindo, Rafael Araújo da Silva!');
+    });
+});
+
